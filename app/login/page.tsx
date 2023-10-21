2a641313a896fcd11eb940dc69aca89b003622bb
theofournier <fournier.theo@gmail.com>
2023-10-21 19:02:09 -0400

    Design login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,8 @@
 import { getSession } from "@/lib/supabase-server";
+import { Card, CardBody } from "@nextui-org/card";
+import { Button } from "@nextui-org/button";
 import { redirect } from "next/navigation";
+import { Input } from "@nextui-org/input";
 
 export default async function Login() {
   const session = await getSession();
@@ -9,12 +12,21 @@ export default async function Login() {
   }
 
   return (
-    <form action="/api/auth/sign-in" method="post">
-      <label htmlFor="email">Email</label>
-      <input name="email" />
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" />
-      <button>Sign In</button>
-    </form>
+    <div className="flex justify-center items-center mt-20 mx-4">
+      <Card isBlurred shadow="sm">
+        <CardBody>
+          <form action="/api/auth/sign-in" method="post">
+            <div className="flex flex-col items-center gap-4">
+              <Input type="email" name="email" placeholder="Email" />
+              <Input type="password" name="password" placeholder="Password" />
+
+              <Button type="submit" color="primary" variant="shadow">
+                Login
+              </Button>
+            </div>
+          </form>
+        </CardBody>
+      </Card>
+    </div>
   );
 }
